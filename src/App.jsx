df403e4af2136ca4d72761d820106553f629087d
vinayak <vinayaksolanke101@gmail.com>
2024-06-04 10:08:15 +0530

    my project

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+// src/Login.js
+import React, { useState } from 'react';
+
+function Login() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [message, setMessage] = useState('');
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+    }
+    return (
+        <div className="form-container">
+            <h2>Welcome!</h2>
+            <form onSubmit={handleLogin}>
+                <input
+                    type="text"
+                    placeholder="Username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    required
+                />
+                <input
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                />
+                <button type="submit">Log In</button>
+            </form>
+            {message && <p>{message}</p>}
+        </div>
+    );
+}
+
+export default Login;
